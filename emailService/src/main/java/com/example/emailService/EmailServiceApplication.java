87cbfed909a2e2955c42dcb2ffd130e55183e27b
Adetunji Samuel <samadetunji01@gmail.com>
2021-08-18 12:50:20 -0500

    chore: setup mail service

--- emailService/src/main/java/com/example/emailService/EmailServiceApplication.java
+++ emailService/src/main/java/com/example/emailService/EmailServiceApplication.java
@@ -6,8 +6,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailServiceApplication {
 
+	@Value("${spring.mail.username}")
+	private String mailUsername;
+
+	@Value("${spring.mail.password}")
+	private String mailPassword;
+
+	@Value("${spring.mail.host}")
+	private String mailHost;
+
+	@Value("#{T(java.lang.Integer).parseInt('${spring.mail.port}')}")
+	private Integer mailPort;
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(EmailServiceApplication.class, args);
 	}
+
+
+
+	
 
 }
